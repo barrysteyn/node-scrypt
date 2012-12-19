3df914064ffc91117e345d31dfa5bf6aa78bb19f
Barry Steyn <barry.steyn@gmail.com>
2012-12-18 20:24:52 -0500

    Refactored password hash code

--- src/passwordhash/scrypthash.h
+++ src/passwordhash/scrypthash.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int HashPassword(const char* passwd, uint64_t N, uint32_t r, uint32_t p, uint8_t header[96]);
+int HashPassword(const uint8_t*, uint8_t header[96], size_t, double, double);
+int VerifyHash(const uint8_t header[96], const uint8_t*);
 
 #endif /* !_SCRYPTHASH_H_ */
