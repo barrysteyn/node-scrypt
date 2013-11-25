76732d7ebda6db38729fd92a606beef2af63e5ac
Barry Steyn <barry.steyn@gmail.com>
2013-11-25 06:07:37 -0800

    #22 Removed encryption/decryption functionality as well as their related tests

--- src/node-boilerplate/scrypt_node_sync.h
+++ src/node-boilerplate/scrypt_node_sync.h
@@ -31,5 +31,3 @@ using namespace v8;
 
 Handle<Value> HashSync(const Arguments& args);
 Handle<Value> VerifySync(const Arguments& args);
-Handle<Value> EncryptSync(const Arguments& args);
-Handle<Value> DecryptSync(const Arguments& args);
