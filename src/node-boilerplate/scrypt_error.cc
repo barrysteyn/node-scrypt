fdcaa678a6396fa66fe9c2ac106054bcde8212dc
Barry Steyn <barry.steyn@gmail.com>
2013-12-20 08:35:44 -0800

    #28 Scrypt error: A means to obtain the error object from JS land

---
+++ src/node-boilerplate/scrypt_error.cc
@@ -0,0 +1,53 @@
+/*
+scrypt_error.cc 
+
+Copyright (C) 2013 Barry Steyn (http://doctrina.org/Scrypt-Authentication-For-Node.html)
+
+This source code is provided 'as-is', without any express or implied
+warranty. In no event will the author be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this source code must not be misrepresented; you must not
+claim that you wrote the original source code. If you use this source code
+in a product, an acknowledgment in the product documentation would be
+appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be
+misrepresented as being the original source code.
+
+3. This notice may not be removed or altered from any source distribution.
+
+Barry Steyn barry.steyn@gmail.com
+
+*/
+
+#include <node.h>
+#include <v8.h>
+#include <string>
+
+using namespace v8;
+
+#include "scrypt_common.h"
+#include "scrypt_error.h"
+
+//
+// Error: Entry point from JavaScript land
+//
+Handle<Value> 
+MakeErrorObject(const Arguments& args) {
+	HandleScope scope;
+	Local<Value> errObj;
+	std::string errString = (args[1]->IsString()) ? std::string(*v8::String::Utf8Value(args[1]->ToString())) : "";
+	const char* errMessage = (errString != "") ? errString.c_str() : NULL; //We should be able to use c_str as it won't be popped off the stack until exiting this function
+	int scryptErrCode = (args[2]->IsNumber()) ? args[2]->ToNumber()->Value() : -1;
+	if (!args[0]->IsNumber()) {
+		errObj = Internal::MakeErrorObject(500, errMessage, scryptErrCode);
+	} else
+		errObj = Internal::MakeErrorObject(args[0]->ToNumber()->Value(), errMessage, scryptErrCode);
+	
+	return scope.Close(errObj);
+}
