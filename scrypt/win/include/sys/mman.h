c74dd07b1441d8368831f498cbe790b55b552d9a
Barry Steyn <barry.steyn@gmail.com>
2014-03-05 16:34:40 -0800

    #17 Removing unnecessary windows files

--- scrypt/win/include/sys/mman.h
+++ scrypt/win/include/sys/mman.h
@@ -6,9 +6,9 @@
 #ifndef _SYS_MMAN_H_
 #define _SYS_MMAN_H_
 
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
+#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.
 #define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
-#endif						
+#endif
 
 /* All the headers include this file. */
 #ifndef _MSC_VER
