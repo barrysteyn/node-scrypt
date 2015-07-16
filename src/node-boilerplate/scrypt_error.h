1891a00af89d99bfcfdc36c7c6ebfecdb2af65eb
Barry Steyn <barry.steyn@gmail.com>
2015-07-16 10:04:37 -0700

    Deleting scrypt_error

--- src/node-boilerplate/scrypt_error.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SCRYPTERROR_H_
-#define _SCRYPTERROR_H_
-
-namespace Scrypt {
-  const char* ScryptErrorDescr(const int error);
-  v8::Local<v8::Value> ScryptError(const int error);
-};
-
-#endif /* _SCRYPTERROR_H_ */
