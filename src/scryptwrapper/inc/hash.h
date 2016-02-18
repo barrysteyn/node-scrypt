52a1cb792bc7ae0e4051ba4ad53e7b60fa5c806d
James Newell <james.newell@gmail.com>
2016-02-18 12:51:05 -0500

    fix the hash/hashSync functions so they take the output of params/paramsSync properly

--- src/scryptwrapper/inc/hash.h
+++ src/scryptwrapper/inc/hash.h
@@ -26,6 +26,9 @@ Barry Steyn barry.steyn@gmail.com
 #define _KEYDERIVATION_H_
 
 unsigned int
+Hash(const uint8_t*, size_t, const uint8_t*, size_t, uint64_t, uint32_t, uint32_t, uint8_t*, size_t);
+
+unsigned int
 ScryptHashFunction(const uint8_t*, size_t, const uint8_t*, size_t, uint64_t, uint32_t, uint32_t, uint8_t*, size_t);
 
 #endif /* !_KEYDERIVATION_H_ */
